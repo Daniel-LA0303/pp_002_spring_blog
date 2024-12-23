ea71b5fb96ac5f8db2ffdfa0b47e58d314b99f9a
Luis Alberto <03daniel030399@gmail.com>
2024-12-23 09:17:20 -0600

    add cros orgin in categories controller

--- src/main/java/com/mx/dev/blog/spring_001_blog/controllers/CategoryController.java
+++ src/main/java/com/mx/dev/blog/spring_001_blog/controllers/CategoryController.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mx.dev.blog.spring_001_blog.entities.ctaegory.CategoryEntity;
@@ -25,6 +27,7 @@ import com.mx.dev.blog.spring_001_blog.utils.validators.CategoryValidator;
 
 @RestController
 @RequestMapping("/api/category")
+@CrossOrigin(origins = "http://localhost:5173", methods = { RequestMethod.GET, RequestMethod.POST })
 public class CategoryController {
 
 	/**
@@ -56,8 +59,7 @@ public class CategoryController {
 	}
 
 	/**
-<<<<<<< HEAD
-=======
+	 * <<<<<<< HEAD =======
 	 * 
 	 * @param ids
 	 * @return
@@ -77,8 +79,7 @@ public class CategoryController {
 	}
 
 	/**
->>>>>>> feature/LAZD-service-category
-	 * get a category by id
+	 * >>>>>>> feature/LAZD-service-category get a category by id
 	 * 
 	 * @param categoryId
 	 * @return
