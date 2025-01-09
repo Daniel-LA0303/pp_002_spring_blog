be24f8dd8f4b45a745bf750493e25583e820c18d
Luis Alberto <03daniel030399@gmail.com>
2025-01-09 10:03:25 -0600

    init UI admin

--- src/main/java/com/mx/dev/blog/spring_001_blog/config/SecurityConfig.java
+++ src/main/java/com/mx/dev/blog/spring_001_blog/config/SecurityConfig.java
@@ -74,6 +74,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // STATELESS
 
 				.and().authorizeRequests().antMatchers(HttpMethod.GET, "/api/**").permitAll()
+				.antMatchers("/css/**", "/js/**").permitAll().antMatchers(HttpMethod.GET, "/auth-ui/**").permitAll()
 				.antMatchers(HttpMethod.PUT, "/api/**").permitAll().antMatchers(HttpMethod.POST, "/api/**").permitAll()
 				.antMatchers("/api/auth/**").permitAll().antMatchers(HttpMethod.DELETE, "/api/**").permitAll()
 				.anyRequest().authenticated().and().cors().configurationSource(corsConfigurationSource());
