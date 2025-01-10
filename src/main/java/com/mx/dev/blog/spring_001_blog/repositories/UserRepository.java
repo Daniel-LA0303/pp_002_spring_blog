8fc91d7888fc465151fc3c314b5914355c5c6860
Luis Alberto <03daniel030399@gmail.com>
2025-01-09 21:28:52 -0600

    add query to get top user

--- src/main/java/com/mx/dev/blog/spring_001_blog/repositories/UserRepository.java
+++ src/main/java/com/mx/dev/blog/spring_001_blog/repositories/UserRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.repository.query.Param;
 import com.mx.dev.blog.spring_001_blog.entities.user.UserEntity;
 import com.mx.dev.blog.spring_001_blog.utils.dtos.user.UserInfoCardDTO;
 import com.mx.dev.blog.spring_001_blog.utils.dtos.user.UserInfoDTO;
+import com.mx.dev.blog.spring_001_blog.utils.dtos.user.UserTopDTO;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
@@ -54,6 +55,21 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
 			    GROUP BY u.userId, ui.bio, ui.work, ui.education, ui.profilePicture, ui.skills, ui.city, ui.website
 			""")
 	Optional<UserInfoDTO> findUserInfoById(@Param("userId") Long userId);
+
+	@Query("""
+			    SELECT new com.mx.dev.blog.spring_001_blog.utils.dtos.user.UserTopDTO(
+			        u.userId,
+			        u.username,
+			        ui.profilePicture,
+			        COUNT(b.blogId)
+			    )
+			    FROM UserEntity u
+			    LEFT JOIN BlogEntity b ON u.userId = b.userId AND b.status = 'PUBLISHED'
+			    LEFT JOIN UserInfoEntity ui ON u.userId = ui.userId
+			    GROUP BY u.userId, u.username, ui.profilePicture
+			    ORDER BY COUNT(b.blogId) DESC
+			""")
+	List<UserTopDTO> getTopUsersByPosts();
 
 	@Query("""
 			    SELECT new com.mx.dev.blog.spring_001_blog.utils.dtos.user.UserInfoCardDTO(
