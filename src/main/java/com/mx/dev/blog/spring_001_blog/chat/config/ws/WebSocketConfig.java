376665e3a0eaea4ea5cbf105bb9805ff05e78051
daniel-la <03daniel030399@gmail.com>
2025-12-13 17:03:28 -0600

    feat: add coments in scoket config

--- src/main/java/com/mx/dev/blog/spring_001_blog/chat/config/ws/WebSocketConfig.java
+++ src/main/java/com/mx/dev/blog/spring_001_blog/chat/config/ws/WebSocketConfig.java
@@ -3,15 +3,22 @@ package com.mx.dev.blog.spring_001_blog.chat.config.ws;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.converter.DefaultContentTypeResolver;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.security.messaging.context.AuthenticationPrincipalArgumentResolver;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -24,12 +31,36 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+	private static final AtomicInteger activeConnections = new AtomicInteger(0);
+
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
 
 		// add resolver for authentication principal to access current user in
 		// controller methods
 		argumentResolvers.add(new AuthenticationPrincipalArgumentResolver());
+	}
+
+	@Override
+	public void configureClientInboundChannel(ChannelRegistration registration) {
+		registration.interceptors(new ChannelInterceptor() {
+			@Override
+			public Message<?> preSend(Message<?> message, MessageChannel channel) {
+				StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+
+				if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+					int current = activeConnections.incrementAndGet();
+					System.out.println("User connected. Active connections: " + current);
+				}
+
+				if (StompCommand.DISCONNECT.equals(accessor.getCommand())) {
+					int current = activeConnections.decrementAndGet();
+					System.out.println("User disconnected. Active connections: " + current);
+				}
+
+				return message;
+			}
+		});
 	}
 
 	/*
